d2097402f7f36d5eeae41a536631e54f7b8c6e30
SemenSavlook <semensavluk@gmail.com>
2023-04-19 18:53:08 +0600

    start app

---
+++ minesweaper.js
@@ -0,0 +1,89 @@
+export var TILE_STATUSES = {
+  HIDDEN: 'hidden',
+  MINE: 'mine',
+  NUMBER: 'number',
+  MARKED: 'marked'
+}
+
+function createBoard(boardSize, numberOfMines) {
+  const board = [];
+  const minePositions = getMinePositions(boardSize, numberOfMines);
+
+  for (let x = 0; x < boardSize; x++) {
+    const row = [];
+    for (let y = 0; y < boardSize; y++) {
+      const element = document.createElement('div');
+      element.dataset.status = TILE_STATUSES.HIDDEN;
+
+      const tile = {
+        element,
+        x,
+        y,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+          return
+        }
+      }
+      row.push(tile)
+    }
+    board.push(row)
+  }
+
+  return board
+}
+
+function markTile(tile) {
+  if (tile.status !== TILE_STATUSES.HIDDEN && tile.status !== TILE_STATUSES.MARKED) {
+    return;
+  }
+
+  if (tile.status === TILE_STATUSES.MARKED) {
+    tile.status = TILE_STATUSES.HIDDEN;
+  } else {
+    tile.status = TILE_STATUSES.MARKED;
+  }
+
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = [];
+
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize)
+    }
+
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.push(position)
+    }
+  }
+  return positions
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+function revealTile(tile) {
+  if (tile.status !== TILE_STATUSES.HIDDEN) {
+    return;
+  }
+
+  if (tile.mine) {
+    tile.status = TILE_STATUSES.MINE;
+    return;
+  }
+
+  tile.status = TILE_STATUSES.NUMBER;
+}
+
+export { createBoard, markTile, revealTile };
---
+++ script.js
@@ -0,0 +1,33 @@
+import { createBoard, markTile, revealTile, TILE_STATUSES } from './minesweaper.js';
+
+const BOARD_SIZE = 10, NUMBER_OF_MINES = 10;
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector('.board');
+const minesLeftText = document.querySelector('[data-mine-count]');
+
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element);
+    tile.element.addEventListener('click', () => {
+      revealTile(tile);
+    });
+    tile.element.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      markTile(tile);
+      listMinesLeft();
+    });
+  })
+});
+
+boardElement.style.setProperty('--size', BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
+
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+  }, 0);
+
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
+}
+// console.log(createBoard(2, 2))
